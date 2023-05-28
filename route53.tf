5b3f1dead7ee49c2379698afa6cbce56eaf6500b
tekore <tekore@tutamail.com>
2023-05-28 15:38:02 +0100

    route53.tf

--- route53.tf
+++ route53.tf
@@ -1,9 +1,13 @@
 # work in progress
 
+#resource "aws_route53_zone" "public" {
+#  name = "example.com"
+#}
+
 #resource "aws_route53_record" "domain" {
-#  zone_id = aws_route53_zone.primary.zone_id
+#  zone_id = aws_route53_zone.public.zone_id
 #  name    = "www."
 #  type    = "A"
 #  ttl     = 300
-#  records = [aws_eip.cloud.public_ip]
+#  records = [aws_eip.eip.public_ip]
 #}
